9f02aa2c7bc4c29389119719874be02071da0ae2
skrypkasv <skrypkasv@gmail.com>
2021-01-04 15:33:10 +0200

    Добавил 4-е задание

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 // import user from './json/user.json';
 // import Profile from './components/Profile/Profile';
-import statisticalData from './json/statistical-data.json';
-import Statistics from './components/Statistics/Statistics';
+// import statisticalData from './json/statistical-data.json';
+// import Statistics from './components/Statistics/Statistics';
 // import friends from './json/friends.json';
 // import FriendList from './components/FriendList/FriendList';
+import transactions from './json/transactions.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 export default function App() {
   return (
     <div className="container">
-      <Statistics title="Upload stats" stats={statisticalData} />
+      {/* <Statistics title="Upload stats" stats={statisticalData} /> */}
       {/* <Statistics stats={statisticalData} /> */}
       {/* <Profile name={user.name} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} /> */}
       {/* <FriendList items={friends} /> */}
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
