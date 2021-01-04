29667697272276af035e59f7537098e3755e96fa
skrypkasv <skrypkasv@gmail.com>
2021-01-04 14:05:30 +0200

    проверка №2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div className="container">
       <Statistics title="Upload stats" stats={statisticalData} />
-      {/* <Statistics stats={statisticalData} /> */}
+      <Statistics stats={statisticalData} />
       {/* <Profile name={user.name} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} /> */}
       {/* <FriendList items={friends} /> */}
     </div>
